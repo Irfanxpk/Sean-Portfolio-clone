a4c0da5650f4d593d48b94c950f632162132e604
Irfan PK <pkirfanx@gmail.com>
2025-09-02 14:09:33 +0530

    lib issue resolved

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import svgr from 'vite-plugin-svgr';
-import viteImagemin from 'vite-plugin-imagemin';
-import viteCompression from 'vite-plugin-compression';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import svgr from "vite-plugin-svgr";
+import viteImagemin from "vite-plugin-imagemin";
+import viteCompression from "vite-plugin-compression";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr(),
-  viteImagemin({ /* default image optim options */ }),
-    viteCompression() // gzip],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    viteImagemin({
+      /* default image optim options */
+    }),
+    viteCompression(), // gzip],
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@utils': path.resolve(__dirname, 'src/utils')
-    }
-  }
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 });
